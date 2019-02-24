fc364af6edffda97a076e63c3f174d5e3f815083
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-02-24 19:12:45 +0100

    Limit available features on the DemuxedConsumableStream itself; encourage using the main StreamDemux instance instead

--- demuxed-consumable-stream.js
+++ demuxed-consumable-stream.js
@@ -7,32 +7,6 @@ class DemuxedConsumableStream extends ConsumableStream {
     this._streamDemux = streamDemux;
   }
 
-  hasConsumer(consumerId) {
-    return this._streamDemux.hasConsumer(this.name, consumerId);
-  }
-
-  getConsumerStats(consumerId) {
-    if (!this.hasConsumer(consumerId)) {
-      return undefined;
-    }
-    return this._streamDemux.getConsumerStats(consumerId);
-  }
-
-  getConsumerStatsList() {
-    return this._streamDemux.getConsumerStatsList(this.name);
-  }
-
-  getBackpressure() {
-    return this._streamDemux.getBackpressure(this.name);
-  }
-
-  getConsumerBackpressure(consumerId) {
-    if (!this.hasConsumer(consumerId)) {
-      return 0;
-    }
-    return this._streamDemux.getConsumerBackpressure(consumerId);
-  }
-
   createConsumer(timeout) {
     return this._streamDemux.createConsumer(this.name, timeout);
   }
